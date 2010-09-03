c33f31a68ff010581d95e7e9928f69dffd189a75
David Brunton <dbrunton@gmail.com>
2010-09-03 08:52:32 -0400

    input prompt

--- gosh.go
+++ gosh.go
@@ -2,11 +2,16 @@ package main
 
 import(
  "fmt"
+ "bufio"
+ "os"
 )
 
 func main() {
  fmt.Println("You've started gosh.")
  fmt.Printf("%s", Prompt())
+ reader := bufio.NewReader(os.Stdin)
+ input, _ := reader.ReadBytes('\n')
+ fmt.Print(input)
 }
 
 func Prompt() (prompt string) {
