2a397115370067fe908b069c269e6f045b265aa1
David Brunton <dbrunton@gmail.com>
2010-09-24 10:40:19 -0400

    some formatting, an exit statement

--- gosh.go
+++ gosh.go
@@ -9,41 +9,41 @@ import(
 )
 
 func main() {
- fmt.Println("You've started gosh. Try typing 'ls'.")
- for {
-  Prompt()
-//  reader := bufio.NewReader(os.Stdin)
-//  input, _ := reader.ReadBytes('\n')
-  input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-  input = chomp(input)
-  command, arg := parse(input)
-  switch {
-   case (command == "cd"):
-    os.Chdir(arg)
-   case (command == "ls"):
-    ls()
-   case true:
-    fmt.Printf("You typed '%s'.\nType something else.\n", input)
-  }
- }
+	for {
+		Prompt()
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input = chomp(input)
+		command, arg := parse(input)
+
+		switch command {
+			case "cd":
+				os.Chdir(arg)
+			case "ls":
+				ls()
+			case "exit":
+				return
+			default:
+				fmt.Printf("You typed '%s'.\nType something else.\n", input)
+		}
+	}
 }
 
 func Prompt() {
- fmt.Print("gosh> ")
- return
+	fmt.Print("gosh> ")
+	return
 }
 
 func ls() {
- files, _ := ioutil.ReadDir(".")
- for _, v := range files {
-  fmt.Printf("%s\n", v.Name)
- }
+	files, _ := ioutil.ReadDir(".")
+	for _, v := range files {
+		fmt.Printf("%s\n", v.Name)
+	}
 }
 
 func chomp(input string) string {
-  rx, _ := regexp.Compile("\n");
-  chompedInput := rx.ReplaceAllString(string(input), "")
-  return chompedInput
+	rx, _ := regexp.Compile("\n");
+	chompedInput := rx.ReplaceAllString(string(input), "")
+	return chompedInput
 }
 
 func parse(input string) (string, string) {
