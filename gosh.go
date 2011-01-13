cf4ffcf1ae46fc2ce3555aeb66d524243d0e1823
David Brunton <dbrunton@gmail.com>
2011-01-13 07:46:01 -0500

    one term version

--- gosh.go
+++ gosh.go
@@ -5,61 +5,36 @@ import(
  "bufio"
  "os"
  "io/ioutil"
- "regexp"
- "manifest"
 )
 
-// test edit from within gosh
-
 func main() {
+	ch := make(chan string)
 	for {
-		Prompt()
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		input = chomp(input)
-		command, arg := parse(input)
+		fmt.Printf("gosh> ")
 
-		switch command {
-			case "manifest":
-				fmt.Printf(manifest.Manifest(arg))
-			case "cd":
-				os.Chdir(arg)
-			case "ls":
-				ls(arg)
-			case "exit", "quit":
-				os.Exit(0)
-			case "vi":
-				pid, _ := os.ForkExec("/usr/bin/vim", []string{``,arg}, os.Environ(), "", []*os.File{os.Stdin, os.Stdout, os.Stderr})
-				os.Wait(pid, 0) 
-			default:
-				fmt.Printf("You typed '%s'.\nType something else.\n", input)
+		// Tokenize it here
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		// Dispatch it here
+		switch input {
+		        case "print\n": fmt.Printf("%s", <-ch)
+			case "ls\n": ls(ch)
+			default: nothing(ch, input)
 		}
 	}
 }
 
-func Prompt() {
-	fmt.Print("gosh> ")
-	return
+func nothing(ch chan string, input string) {
+	go func(){ ch <- fmt.Sprintf("not implemented!\n"); }()
 }
 
-func ls(dir string) {
-	if dir == `` { dir = "."; }
-	files, _ := ioutil.ReadDir(dir)
-	for _, v := range files {
-		fmt.Printf("%s\n", v.Name)
-	}
-}
-
-func chomp(input string) string {
-	rx, _ := regexp.Compile("\n");
-	chompedInput := rx.ReplaceAllString(string(input), "")
-	return chompedInput
-}
-
-func parse(input string) (string, string) {
-	rx := regexp.MustCompile("[^ ]*")
-	output := rx.FindAllString(input, 2)
-	if(len(output) == 1) {
-		return output[0], ""
-	}
-	return output[0], output[1]
+func ls(ch chan string) {
+	go func(){
+		files, _ := ioutil.ReadDir(".")
+		ret := ""
+		for _, v := range files {
+			ret += fmt.Sprintf("%s\n", v.Name)
+		}
+		ch <- ret
+	}()
 }
